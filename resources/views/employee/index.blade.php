a117c3913eee2cf8209bbc984e2bf2faae871070
Rajieeee <rajtuyay24@gmail.com>
2025-05-26 14:22:29 +0800

    Updated Index File

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -39,6 +39,7 @@
                                 <th>Age</th>
                                 <th>Address</th>
                                 <th>Zip</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
 
@@ -53,14 +54,10 @@
                                 <td>{{$items->age}}</td>
                                 <td>{{$items->address}}</td>
                                 <td>{{$items->zip}}</td>
-                                 <td> 
-
+                                 <td>
                                     <span class="badge bg-success"><a href="{{  route('employee.edit', $items->id)}}" class="btn btn-success mx-3  "><h5>Edit</h5></a></span>
-                                </td>
-                                <td> 
                                     <span class="badge bg-danger"><a href="{{  route('employee.delete', $items->id)}}" class="m-3 p-lg-5"><h5>Delete</h5></a></span>
-                                </td> 
-                                
+                                </td>
                             </tr>
                             @endforeach
                         </tbody>
